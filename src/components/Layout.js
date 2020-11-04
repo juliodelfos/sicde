7eae9edb834fff0bf995c4d2a25fe65613cc7ff4
juliodelfos <yersonolivares@protonmail.com>
2020-11-04 11:15:21 -0300

    Revert "Favicon"
    This reverts commit 95c86915a87893326ed569c6ed279f3bc392dc7f.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,11 +16,6 @@ export default class Body extends React.Component {
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt', null) || _.get(this.props, 'pageContext.site.siteMetadata.description', null)}/>
                     <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i%26Display=swap" rel="stylesheet"/> 
-                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
-                    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
-                    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
-                    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
-                    <link rel="manifest" href="/images/favicon/site.webmanifest"></link>
                 </Helmet>
                 <div id="page" className="site">
                   <Header {...this.props} />
