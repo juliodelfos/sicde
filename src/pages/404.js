1fe6ebd3e90930b562320cfdf78bbf22ce81afde
Yerson <yersonolivares@protonmail.com>
2021-11-18 15:40:08 -0300

    Se crea página 404

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default function HTML() {
+  return (
+    <html>
+      <h1>Página no encontrada</h1>
+      <Link to="/">Volver al inicio</Link>
+    </html>
+  );
+}
