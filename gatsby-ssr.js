11a959cd81c71fdcdaee725e85f4640b5e52dedd
Yerson <yersonolivares@protonmail.com>
2022-01-28 15:56:29 -0300

    Se añade Google Analytics
    Se añade por script. Se elimina paquete.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,20 +7,22 @@
 const React = require("react");
 const withPrefix = require("./src/utils/withPrefix").default;
 
-exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
+exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
+  setHeadComponents([
+    <script>
+      window.ga=window.ga||function(){(ga.q = ga.q || []).push(arguments)}
+      ;ga.l=+new Date; ga('create', 'UA-12409780-1', 'auto'); ga('send',
+      'pageview');
+    </script>,
+    <script async src='https://www.google-analytics.com/analytics.js'></script>
+  ]);
 
-    setHeadComponents([
-        
-    ]);
-
-    setPostBodyComponents([
-        <React.Fragment>
-            <script src={withPrefix('assets/js/plugins.js')}/>
-            <script src={withPrefix('assets/js/main.js')}/>
-            <script src={withPrefix('assets/js/page-load.js')}/>
-            <script src={withPrefix('assets/js/page-unload.js')}/>
-            
-        </React.Fragment>
-    ]);
-
+  setPostBodyComponents([
+    <React.Fragment>
+      <script src={withPrefix("assets/js/plugins.js")} />
+      <script src={withPrefix("assets/js/main.js")} />
+      <script src={withPrefix("assets/js/page-load.js")} />
+      <script src={withPrefix("assets/js/page-unload.js")} />
+    </React.Fragment>,
+  ]);
 };
